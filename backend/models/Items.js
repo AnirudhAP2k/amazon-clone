74045c98f24635e2efc045c31a3d9fd1071f134f
AnirudhAP2k <anirudh0976@gmail.com>
2023-09-20 16:58:07 +0530

    Added timestamp attribute

--- backend/models/Items.js
+++ backend/models/Items.js
@@ -1,26 +1,30 @@
 import mongoose from "mongoose";
 
 const itemSchema = mongoose.Schema({
-        title: {
-            type: String,
-            required: true
-        },
+    title: {
+        type: String,
+        required: true
+    },
 
-        description: {
-            type: String,
-            required: true
-        },
-        
-        price: {
-            type: Number,
-            requires: true
-        },
+    description: {
+        type: String,
+        required: true
+    },
 
-        category: {
-            type: String,
-            required: true
-        }
-});
+    price: {
+        type: Number,
+        requires: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    }
+},
+    {
+        timestamps: true
+    }
+);
 
 const Item = mongoose.model('Item', itemSchema)
 
